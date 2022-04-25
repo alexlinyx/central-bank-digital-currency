6794b479b414862e4436fb26f7a6465a26004b24
ayl57 <ayl57@cornell.edu>
2022-04-25 11:14:52 -0400

    create tables

--- cloud/setup.py
+++ cloud/setup.py
@@ -1,9 +1,9 @@
 import boto3
 import json
 
-with open('../keys.txt') as f:
-    public_key = f.readline()
-    private_key = f.readline
+with open('keys.txt') as f:
+    public_key = f.readline().strip()
+    private_key = f.readline().strip()
 
 client = boto3.client(
     'dynamodb',
@@ -18,3 +18,43 @@ dynamodb = boto3.resource(
     aws_secret_access_key=private_key,
     )
 ddb_exceptions = client.exceptions
+
+wallet_table = dynamodb.create_table(
+    TableName='users',
+    KeySchema=[
+        {
+            'AttributeName': 'address',
+            'KeyType': 'HASH'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'address',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+chain_table = dynamodb.create_table(
+    TableName='chain1',
+    KeySchema=[
+        {
+            'AttributeName': 'sender',
+            'KeyType': 'HASH'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'sender',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
