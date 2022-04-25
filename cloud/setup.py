721abcc6f2f000288c524712a5d05f525f1272b5
ayl57 <ayl57@cornell.edu>
2022-04-25 09:22:23 -0400

    more setup

--- cloud/setup.py
+++ cloud/setup.py
@@ -1,24 +1,20 @@
 import boto3
 import json
 
+with open('../keys.txt') as f:
+    public_key = f.readline()
+    private_key = f.readline
+
 client = boto3.client(
     'dynamodb',
     region_name='us-east-1',
-    aws_access_key_id='AKIAZTI3VFEI747SZTGA',
-    aws_secret_access_key='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',
+    aws_access_key_id=public_key,
+    aws_secret_access_key=private_key
     )
 dynamodb = boto3.resource(
     'dynamodb',
     region_name='us-east-1',
-    aws_access_key_id='AKIAZTI3VFEI747SZTGA',
-    aws_secret_access_key='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',
+    aws_access_key_id=public_key,
+    aws_secret_access_key=private_key,
     )
 ddb_exceptions = client.exceptions
-
-ddb_table = dynamodb.Table('blockchainDB')
-
-
-item = {'wallets':'testing'}
-ddb_table.put_item(
-        Item=item
-        )
