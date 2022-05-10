ac82e07d44db253cb5c53775768f60af14f9d8af
ayl57 <ayl57@cornell.edu>
2022-05-10 13:59:09 -0400

    more experiments

--- combined/tests.py
+++ combined/tests.py
@@ -6,45 +6,45 @@ from datetime import datetime
 from random import randint
 from threading import Thread
 
-chain = Blockchain()
-
 def init(n):
     wallets = []
+    chains = []
     for i in range(n):
         wallets.append(Wallet(i, balance=1000))
-    chain = Blockchain()
-    return wallets
+    chains.append(Blockchain())
+    return wallets, chains
 
-def send(senderWallet, receipient, amount=1):
+def send(senderWallet, receipient, chain, amount=1):
     data = newTransaction(senderWallet, receipient, amount)
     chain.addBlock(data)
+    
 
 def test1(n):
-    wallets = init(10)
+    wallets, chains = init(10)
 
     threads = []
     for _ in range(n):
         i, j = randint(0,9), randint(0,9)
-        threads.append(Thread(target=send, args=(wallets[i],wallets[j],1)))
+        threads.append(Thread(target=send, args=(wallets[i],wallets[j],chains[0],1)))
     
     for t in threads:
         t.start()
 
 
 def test2(n):
-    wallets = init(n)
+    wallets, chains = init(n)
     
     threads = []
     for _ in range(10):
         i, j = randint(0,n-1), randint(0,n-1)
-        threads.append(Thread(target=send, args=(wallets[i],wallets[j],1)))
+        threads.append(Thread(target=send, args=(wallets[i],wallets[j],chains[0],1)))
     
     for t in threads:
         t.start()
 
 
-f1 = open('output1.txt', 'w')
-f2 = open('output2.txt', 'w')
+f1 = open('output3.txt', 'w')
+f2 = open('output4.txt', 'w')
 
 for i in range(100):
     start = datetime.now()
